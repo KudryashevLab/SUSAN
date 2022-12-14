25231c9a9fa6254971d36d670a2a66104aee4176
Artsemi Yushkevich <artsemi.yushkevich@mdc-berlin.de>
2022-12-14 12:53:03 +0100

    remove modified tutorial

--- +SUSAN/tutorial/data/download_data.sh
+++
@@ -1,4 +0,0 @@
-wget http://ftp.ebi.ac.uk/empiar/world_availability/10064/data/mixedCTEM_tomo1.mrc
-wget http://ftp.ebi.ac.uk/empiar/world_availability/10064/data/mixedCTEM_tomo2.mrc
-wget http://ftp.ebi.ac.uk/empiar/world_availability/10064/data/mixedCTEM_tomo3.mrc
-wget http://ftp.ebi.ac.uk/empiar/world_availability/10064/data/mixedCTEM_tomo4.mrc
